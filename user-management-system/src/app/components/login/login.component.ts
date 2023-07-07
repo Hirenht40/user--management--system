d0a8ee757d712940ea7761d46fbd62ff70a4ff79
Hirenkumar <hirenkumarht40@gmail.com>
2023-07-07 16:05:26 +0530

    commit

--- user-management-system/src/app/components/login/login.component.ts
+++ user-management-system/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient) { }
+
+  login(): void {
+    const credentials = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.http.post('http://localhost:3000/login', credentials)
+      .subscribe(
+        () => {
+          alert('Logged in successfully!');
+        },
+        (error) => {
+          console.error(error);
+          alert('Login failed. Please try again.');
+        }
+      );
+  }
 }
