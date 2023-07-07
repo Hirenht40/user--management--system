ce10956d09ae92c234aed62682472d98e9a0598a
Hirenkumar <hirenkumarht40@gmail.com>
2023-07-07 15:54:20 +0530

    commit

--- user-management-system/src/app/components/user-list/user-list.component.ts
+++ user-management-system/src/app/components/user-list/user-list.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent{
+  users: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    this.http.get<any[]>('http://localhost:3000/users')
+      .subscribe(
+        (response) => {
+          this.users = response;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
 }
