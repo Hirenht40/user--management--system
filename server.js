e086dde4dd164d05bbb3559edc8312db88f24bbd
Hirenkumar <hirenkumarht40@gmail.com>
2023-07-10 09:48:11 +0530

    commit

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
 const app = express();
+const path = require("path");
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-mongoose.connect('mongodb://localhost:27017/userdb', {
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI), ({
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -18,6 +23,13 @@ db.once('open', () => {
 });
 
 app.use('/', userRoutes);
+
+//static files
+app.use(express.static(path.join(__dirname, "./user-management-system/dist")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./user-management-system/dist/user-management-system/index.html"));
+});
 
 const port = 3000;
 app.listen(port, () => {
